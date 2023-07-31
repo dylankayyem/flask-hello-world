de480741e71c30b7d1a7c4872b974982d07f77f1
Dylan Kayyem <dyga6971@colorado.edu>
2023-07-31 22:43:36 +0000

    added db_test to app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def testing():
+	conn = psycopg2.connect("postgres://mypostgresql_25z3_user:EzQfeinYSvvORXtwG96xFoL6F1os5OSN@dpg-cj43gbh8g3nakvhhg5gg-a/mypostgresql_25z3")
+	conn.close()
+	return "Database Connection Successful!"
