c104162cb3a57028837262d581e49b3b6d1017c1
Dylan Kayyem <dyga6971@colorado.edu>
2023-07-31 22:59:00 +0000

    added db_insert, db_select, and db_drop

--- app.py
+++ app.py
@@ -17,15 +17,60 @@ def creating():
 	conn = psycopg2.connect("postgres://mypostgresql_25z3_user:EzQfeinYSvvORXtwG96xFoL6F1os5OSN@dpg-cj43gbh8g3nakvhhg5gg-a/mypostgresql_25z3")
 	cur = conn.cursor()
 	cur.execute('''
-    CREATE TABLE IF NOT EXISTS Basketball(
+		CREATE TABLE IF NOT EXISTS Basketball(
 		First varchar(255),
 		Last varchar(255),
 		City varchar(255),
 		Name varchar(255),
 		Number int
 		);
-	''')
+		''')
 	conn.commit()
 	conn.close()
 	return "Basketball Table Succesfully Created"
+
+@app.route('/db_insert')
+def inserting():
+	conn = psycopg2.connect("postgres://mypostgresql_25z3_user:EzQfeinYSvvORXtwG96xFoL6F1os5OSN@dpg-cj43gbh8g3nakvhhg5gg-a/mypostgresql_25z3")
+	cur = conn.cursor()
+	cur.execute('''
+		INSERT INTO Basketball (First, Last, City, Name, Number)
+		Values
+		('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+		('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+		('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+		('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+		''')
+	conn.commit()
+	conn.close()
+	return "Basketball Table Succesfully Populated"
+
+@app.route('/db_select')
+def selecting():
+	conn = psycopg2.connect("postgres://mypostgresql_25z3_user:EzQfeinYSvvORXtwG96xFoL6F1os5OSN@dpg-cj43gbh8g3nakvhhg5gg-a/mypostgresql_25z3")
+	cur = conn.cursor()
+	cur.execute('''
+		SELECT * FROM Basketball;
+		''')
+	records = cur.fetchall()
+	conn.close()
+	response_string = " "
+	response_string +="<table>"
+	for player in records:
+		response_string += "</tr>"
+		for info in player:
+			response_string+="<td>{}</td>".format(info)
+		response_string+="</tr>"
+	response_string+="</table>"
+	return response_string
 	
+@app.route('/db_drop')
+def dropping():
+	conn = psycopg2.connect("postgres://mypostgresql_25z3_user:EzQfeinYSvvORXtwG96xFoL6F1os5OSN@dpg-cj43gbh8g3nakvhhg5gg-a/mypostgresql_25z3")
+	cur = conn.cursor()
+	cur.execute('''
+		DROP TABLE Basketball;
+		''')
+	conn.commit()
+	conn.close()
+	return "Basketball Table Succesfully Dropped"
