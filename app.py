fc236482921d2c164172c323cd947f521bb39663
Dylan Kayyem <dyga6971@colorado.edu>
2023-07-31 22:52:08 +0000

    added db_create

--- app.py
+++ app.py
@@ -16,15 +16,15 @@ def testing():
 def creating():
 	conn = psycopg2.connect("postgres://mypostgresql_25z3_user:EzQfeinYSvvORXtwG96xFoL6F1os5OSN@dpg-cj43gbh8g3nakvhhg5gg-a/mypostgresql_25z3")
 	cur = conn.cursor()
-	cur.execute( ' ' '
-		CREATE TABLE IF NOT EXISTS Basketball (
+	cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
 		First varchar(255),
 		Last varchar(255),
 		City varchar(255),
 		Name varchar(255),
 		Number int
 		);
-		' ' ' )
+	''')
 	conn.commit()
 	conn.close()
 	return "Basketball Table Succesfully Created"
